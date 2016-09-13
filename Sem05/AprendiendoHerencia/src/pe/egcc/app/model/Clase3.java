b0c5a178a998be5bef956ed1594da39bcc05a128
Docente UCH <docente@soporteuch.pe>
2016-09-12 20:18:46 -0500

    Sem05

---
+++ Sem05/AprendiendoHerencia/src/pe/egcc/app/model/Clase3.java
@@ -0,0 +1,14 @@
+package pe.egcc.app.model;
+
+/**
+ *
+ * @author Eric Gustavo Coronel Castillo
+ * @blog gcoronelc.blogspot.com
+ * @email gcoronelc@gmail.com
+ */
+public class Clase3 extends Clase2{
+  
+  public void metodo6(){
+    
+  }
+}
